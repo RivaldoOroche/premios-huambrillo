ec92b53ab7ce5f1cfee21aae60b2354f51a14e20
RivaldoOroche <35975107+RivaldoOroche@users.noreply.github.com>
2026-04-20 22:04:07 -0500

    Actualizado Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Sorteos from "./components/Sorteos";
+import AlertaSeguridad from "./components/AlertaSeguridad";
+import Ganadores from "./components/Ganadores";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +11,9 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Sorteos />
+      <AlertaSeguridad />
+      <Ganadores />
+      <Footer />
     </main>
   );
 }
